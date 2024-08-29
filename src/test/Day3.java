74a22ad12657aac1dcfec6dad234fcef510fa13b
tusharfiet24 <ertusharfiet24@gmail.com>
2024-08-29 13:02:45 +0530

    Prioritizing the test case

---
+++ src/test/Day3.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day3 {
+	@Test
+	public void webLoginCarLogin() {
+		//Selenium
+		System.out.println("weblogincar");
+	}
+	
+	@Test
+	public void mobileLoginCarLogin() {
+		//Appium
+		System.out.println("mobilelogincar");
+	}
+	
+	@Test
+	public void LoginAPIcarLoan() {
+		//Rest API automation
+		System.out.println("apilogincar");
+	}
+}
---
+++ src/test/Day4.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day4 {
+	@Test
+	public void webLoginHomeLogin() {
+		//Selenium
+		System.out.println("webloginhome");
+	}
+	
+	@Test
+	public void mobileLoginHomeLogin() {
+		//Appium
+		System.out.println("mobileloginhome");
+	}
+	
+	@Test
+	public void LoginAPIhomeLoan() {
+		//Rest API automation
+		System.out.println("apiloginhome");
+	}
+}
