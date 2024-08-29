86b0661cabe894ca8d8496552a83ba737377ad77
tusharfiet24 <ertusharfiet24@gmail.com>
2024-08-29 18:16:44 +0530

    Refactor the code

--- src/test/Day3.java
+++ src/test/Day3.java
@@ -11,90 +11,93 @@ import org.testng.annotations.Test;
 
 public class Day3 {
 //	TestNG Helper Attributes -> dependsOnMethods, enabled, timeOut.
-	
+
 	@BeforeClass
 	public void beforeClass() {
 		System.out.println("Before executing any method in the class");
 	}
-	
-	@Parameters({"URL", "APIKey/username"})
+
+	@Parameters({ "URL", "APIKey/username" })
 	@Test
 	public void webLoginCarLogin(String urlname, String key) {
-		//Selenium
+		// Selenium
 		System.out.println("weblogincar");
 		System.out.println(urlname);
 		System.out.println(key);
 	}
-	
-	@Test(groups= {"Smoke"})
+
+	@Test(groups = { "Smoke" })
 	public void mobileLoginCarLogin() {
-		//Appium
+		// Appium
 		System.out.println("mobilelogincar");
 	}
-	
+
 	@BeforeSuite
 	public void beforeSuite() {
-		//Appium
+		// Appium
 		System.out.println("I'll execute Before test suite");
 	}
-	
-	@Test(enabled=false)
+
+	@Test(enabled = false)
 	public void mobileSignInCarLogin() {
-		//Appium
+		// Appium
 		System.out.println("mobilesignincar");
 	}
-	
-	@Test(timeOut=4000, dataProvider="getData")
+
+	@Test(timeOut = 4000, dataProvider = "getData")
 	public void mobileSignOutCarLogin(String username, String password) throws InterruptedException {
-		//Appium
+		// Appium
 		Thread.sleep(3000);
 		System.out.println("mobilesignoutcar");
 		System.out.println(username);
 		System.out.println(password);
 	}
-	
+
 	@BeforeMethod
 	public void beforeMethod() {
-		//Appium
+		// Appium
 		System.out.println("I'll execute before every method in Day3 class");
 	}
-	
-	@Test(dependsOnMethods={"mobileSignOutCarLogin", "webLoginCarLogin",})
+
+	@Test(dependsOnMethods = { "mobileSignOutCarLogin", "webLoginCarLogin", })
 	public void APIcarLoan() {
-		//Rest API automation
+		// Rest API automation
 		System.out.println("apilogincar");
 	}
-	
+
 	@AfterMethod
 	public void afterMethod() {
-		//Appium
+		// Appium
 		System.out.println("I'll execute after every method in Day3 class");
 	}
-	
+
 	@AfterClass
 	public void afterClass() {
 		System.out.println("Before executing all methods in the class");
 	}
-	
+
 	@DataProvider
-	public Object[][] getData(){
-		//1st combination - username password - good credit history = row
-		//2nd - username password - no credit history
-		//3rd - fraudelent credit history
-		Object[][] data = new Object[3][2];
-		//1st set
-		data[0][0] = "firstsetusername";
-		data[0][1] = "password";
-		//columns in the row are nothing but values for that particular combination(row)
-		
-		//2nd set
-		data[1][0] = "secondsetusername";
-		data[1][1] = "password";
-		
-		//3rd set
-		data[2][0] = "thirdsetusername";
-		data[2][1] = "password";
-		
+	public Object[][] getData() {
+		// 1st combination - username password - good credit history = row
+		// 2nd - username password - no credit history
+		// 3rd - fraudelent credit history
+//		Object[][] data = new Object[3][2];
+//		//1st set
+//		data[0][0] = "firstsetusername";
+//		data[0][1] = "password";
+//		//columns in the row are nothing but values for that particular combination(row)
+//		
+//		//2nd set
+//		data[1][0] = "secondsetusername";
+//		data[1][1] = "password";
+//		
+//		//3rd set
+//		data[2][0] = "thirdsetusername";
+//		data[2][1] = "password";
+
+		Object[][] data = { { "firstsetusername", "password" }, { "secondsetusername", "password" },
+				{ "thirdsetusername", "password" } };
+
 		return data;
 	}
 }
