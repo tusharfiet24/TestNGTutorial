f59ecc7ec94f304bee59c79285045383af129ed8
tusharfiet24 <ertusharfiet24@gmail.com>
2024-08-29 12:48:12 +0530

    Setup testng.xml file in TestNG config

--- src/test/Day1.java
+++ src/test/Day1.java
@@ -5,6 +5,16 @@ import org.testng.annotations.Test;
 public class Day1 {
 	@Test
 	public void demo() {
-		System.out.println("hello");
+		System.out.println("Tushar");
+	}
+	
+	@Test
+	public void firstTest() {
+		System.out.println("Hello");
+	}
+	
+	@Test
+	public void secondTest() {
+		System.out.println("World");
 	}
 }
---
+++ src/test/Day2.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day2 {
+	@Test
+	public void pLoan() {
+		System.out.println("Personal Loan");
+	}
+}
