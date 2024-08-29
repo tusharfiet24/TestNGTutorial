6a16ab32922383dbc4ebe73a27c2840b3c2377a8
tusharfiet24 <ertusharfiet24@gmail.com>
2024-08-29 18:10:36 +0530

    Parameterization using DataProvider in TestNG

--- src/test/Day3.java
+++ src/test/Day3.java
@@ -5,6 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -16,12 +17,13 @@ public class Day3 {
 		System.out.println("Before executing any method in the class");
 	}
 	
-	@Parameters({"URL"})
+	@Parameters({"URL", "APIKey/username"})
 	@Test
-	public void webLoginCarLogin(String urlname) {
+	public void webLoginCarLogin(String urlname, String key) {
 		//Selenium
 		System.out.println("weblogincar");
 		System.out.println(urlname);
+		System.out.println(key);
 	}
 	
 	@Test(groups= {"Smoke"})
@@ -42,11 +44,13 @@ public class Day3 {
 		System.out.println("mobilesignincar");
 	}
 	
-	@Test(timeOut=4000)
-	public void mobileSignOutCarLogin() throws InterruptedException {
+	@Test(timeOut=4000, dataProvider="getData")
+	public void mobileSignOutCarLogin(String username, String password) throws InterruptedException {
 		//Appium
 		Thread.sleep(3000);
 		System.out.println("mobilesignoutcar");
+		System.out.println(username);
+		System.out.println(password);
 	}
 	
 	@BeforeMethod
@@ -70,5 +74,27 @@ public class Day3 {
 	@AfterClass
 	public void afterClass() {
 		System.out.println("Before executing all methods in the class");
+	}
+	
+	@DataProvider
+	public Object[][] getData(){
+		//1st combination - username password - good credit history = row
+		//2nd - username password - no credit history
+		//3rd - fraudelent credit history
+		Object[][] data = new Object[3][2];
+		//1st set
+		data[0][0] = "firstsetusername";
+		data[0][1] = "password";
+		//columns in the row are nothing but values for that particular combination(row)
+		
+		//2nd set
+		data[1][0] = "secondsetusername";
+		data[1][1] = "password";
+		
+		//3rd set
+		data[2][0] = "thirdsetusername";
+		data[2][1] = "password";
+		
+		return data;
 	}
 }
